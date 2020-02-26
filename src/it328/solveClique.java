89afec00b171bfc15077a46a3f401a2abd108f41
Rumbold, Dayton <rumboldayton@gmail.com>
2020-02-26 16:32:34 -0600

    Find maximum clique and modified file input

--- src/it328/solveClique.java
+++ src/it328/solveClique.java
@@ -45,6 +45,36 @@ public class solveClique {
 		setEdges();
 	}
 	
+	 public ArrayList<Integer> findMaxClique(ArrayList<Integer> clique, int row, int vertices) {
+	        long start = System.currentTimeMillis();
+
+	        ArrayList<Integer> tempClique = new ArrayList<Integer>();
+	        ArrayList<Integer> maxClique = new ArrayList<Integer>();
+
+	        maxClique = clique;
+
+	        for (int i = row; i < vertices; i++) {
+	            boolean isClique = true;
+	            for (int j = 0; j < clique.size(); j++) {
+	                if (matrix[clique.get(j)][i] != 1) {
+	                    isClique = false;
+	                }
+	            }
+
+	            if (isClique) {
+	                ArrayList<Integer> currClique = new ArrayList<Integer>(clique);
+	                currClique.add(i);
+	                tempClique = findMaxClique(currClique, i + 1, vertices);
+
+	                if (tempClique.size() > maxClique.size())
+	                    maxClique = tempClique;
+	            }
+	        }
+	        long end = System.currentTimeMillis();
+	        time = end - start;
+	        return maxClique;
+	    }
+	 
 	public int edgeCount() {//edge count getter
 		return edges;
 	}
@@ -63,7 +93,7 @@ public class solveClique {
 	/*
 	 * To do: finish creation of graph
 	 */
-	public void readGraph(String fname) {
+	public void readGraph(solveClique clique, String fname) {
 try {
 			BufferedReader br= new BufferedReader(new FileReader(fname));
 			String line= null;
@@ -86,14 +116,14 @@ try {
 	                    }
 
 	                    ArrayList<Integer> cliqueAL = new ArrayList<Integer>();
-	                   // cliqueAL = clique.findMaxClique(cliqueAL, row, vertices);
+	                   cliqueAL = clique.findMaxClique(cliqueAL, row, vertices);
 	                  //  print(cliqueAL);
 	                }
 	            }
 	            br.close();
 		}
 		catch(FileNotFoundException fnf) {
-			System.out.println("File Not Located");
+			System.out.println("File not found");
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -117,14 +147,14 @@ try {
 	 */
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		String fname= args[0];
-		
+		//String fname= args[0]; //change when using on Linux server
+		String fname= "graphs2019.txt";
 		solveClique clique= new solveClique();
 		
 		int edges = 0;
 		int totalEdge = 0;
-	
-		clique.readGraph(fname);
+		
+		clique.readGraph(clique,fname);
 	}
 
 }
